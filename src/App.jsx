c4f3052935c05d085aeb59eb016f608880cf1e46
Mesud-Ahmed <mesudahmed94@gmail.com>
2025-02-23 10:17:02 +0300

    feat: add a section for doctors

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Navbar from "./components/Navbar"
 import Home from "./components/Home"
 import About from "./components/About"
+import Services from "./components/Services"
+import Doctors from "./components/Doctors"
 function App() {
 
 
@@ -13,6 +15,12 @@ function App() {
         </div>
         <div id="about">
           <About/>
+        </div>
+        <div id="services">
+          <Services/>
+        </div>
+        <div id="doctors">
+          <Doctors/>
         </div>
       </main>
     </>
