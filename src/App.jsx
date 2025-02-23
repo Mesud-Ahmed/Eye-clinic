99df0312a7d876f26011c8db2cb28521dc6bdd7a
Mesud-Ahmed <mesudahmed94@gmail.com>
2025-02-23 08:35:17 +0300

    feat: add responsive  navbar for smooth navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-
+import Navbar from "./components/Navbar"
 
 function App() {
 
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">hellllo brother</h1>
+      <Navbar/>
     </>
   )
 }
