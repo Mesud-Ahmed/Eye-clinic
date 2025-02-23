fede9bf649f424b40ef082b6d1caddd39c619406
Mesud-Ahmed <mesudahmed94@gmail.com>
2025-02-23 08:49:37 +0300

    feat: add headline and background image for the top section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import Navbar from "./components/Navbar"
-
+import Home from "./components/Home"
 function App() {
 
 
   return (
     <>
       <Navbar/>
+      <main>
+        <div id="home">
+          <Home/>
+        </div>
+      </main>
     </>
   )
 }
